952bdc83b411e932f309c38ba34766f7b1a6a3ff
Henri Uitenweerde <rhino888@gmail.com>
2023-06-10 14:51:40 +0200

    Added the first line transition

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="mainstyles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSS Components Home</title>
 </head>
 <body>
-    
+    <article>
+        <h1>Line Transition</h1>
+        <a href="aio-css-files/line-transition.html">Preview</a>
+        <p>This adds a nice line transition to any element on hover. More options can probably be used.</p>
+        <p>Found this on <a href="https://www.youtube.com/@KevinPowell">Kevin Powel's Youtube Channel</a>
+    </article>
 </body>
 </html>
