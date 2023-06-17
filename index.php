670116729649a421e29fcb44d3f859ef1c944590
Henri Uitenweerde <rhino888@gmail.com>
2023-06-17 20:12:24 +0200

    Styled the home page

--- index.php
+++ index.php
@@ -3,24 +3,34 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css-reset.css">
     <link rel="stylesheet" href="mainstyles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSS Components Home</title>
 </head>
 
 <body>
-    <article>
-        <h1>Button - Line Transition</h1>
-        <a href="aio-css-files/line-transition.html">Preview</a>
-        <p>This adds a nice line transition to any element on hover. More options can probably be used.</p>
-        <p>Found this on <a href="https://www.youtube.com/@KevinPowell">Kevin Powel's Youtube Channel</a>
-    </article>
-    <article>
-        <h1>Buttone - Awesome</h1>
-        <a href="aio-css-files/awesome-btn.html">Preview</a>
-        <p>This adds a nice effect to any element on hover. More options can probably be used.</p>
-        <p>Found this on <a href="https://www.youtube.com/watch?v=0px6YH-cauQ">Improve your CSS by Keepin' it DRY</a>
-    </article>
+    <header>
+        <h1>CSS Components Home Page</h1>
+    </header>
+    <h2>Buttons</h2>
+    <section>
+        <article>
+            <h3>Button - Awesome</h3>
+            <a href="aio-css-files/buttons/awesome-btn.html">Preview</a>
+            <p>This adds a nice effect to any element on hover. More options can probably be used.</p>
+            <p>Found this on <a href="https://www.youtube.com/watch?v=0px6YH-cauQ">Improve your CSS by Keepin' it DRY</a>
+        </article>
+    </section>
+    <h2>Navigation</h2>
+    <section>
+        <article>
+            <h3>Button - Line Transition</h3>
+            <a href="aio-css-files/nav/line-transition.html">Preview</a>
+            <p>This adds a nice line transition to any element on hover. More options can probably be used.</p>
+            <p>Found this on <a href="https://www.youtube.com/@KevinPowell">Kevin Powel's Youtube Channel</a>
+        </article>
+    </section>
 </body>
 
 </html>
