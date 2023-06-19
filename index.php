cb09671468e2d46fcecb0102cc27b5762a22f81d
Henri Uitenweerde <rhino888@gmail.com>
2023-06-19 20:46:59 +0200

    Added more components

--- index.php
+++ index.php
@@ -15,11 +15,38 @@
     </header>
     <h2>Buttons</h2>
     <section>
+    <article>
+            <h3>3D Flip Button</h3>
+            <a href="aio-css-files/buttons/3d-flip-btn.html">Preview</a>
+            <p>In this video we will walk-through how to create a 3D button that flips to another side when hovered.</p>
+            <p>Found this on <a href="https://www.youtube.com/watch?v=_isJ_SpN7T0">3D Flip Button Tutorial</a>
+        </article>
         <article>
             <h3>Button - Awesome</h3>
             <a href="aio-css-files/buttons/awesome-btn.html">Preview</a>
             <p>This adds a nice effect to any element on hover. More options can probably be used.</p>
             <p>Found this on <a href="https://www.youtube.com/watch?v=0px6YH-cauQ">Improve your CSS by Keepin' it DRY</a>
+        </article>
+    </section>
+    <h2>Image Transitions</h2>
+    <section>
+    <article>
+            <h3>B&W to Color</h3>
+            <a href="aio-css-files/images/reveal-color.html">Preview</a>
+            <p>Images on the page change from black and white to color on hover.</p>
+            <p>Found this on <a href="https://www.sitepoint.com/css-image-reveal-animation/">sitepoint article</a>
+        </article>
+        <article>
+            <h3>Reveal from All Sides</h3>
+            <a href="aio-css-files/images/reveal-all-sides.html">Preview</a>
+            <p>Images on the page is hidden, and reveals on hover.</p>
+            <p>Found this on <a href="https://www.sitepoint.com/css-image-reveal-animation/">sitepoint article</a>
+        </article>
+        <article>
+            <h3>Reveal from Directions</h3>
+            <a href="aio-css-files/images/reveal-directions.html">Preview</a>
+            <p>Images on the page is hidden, and reveals on hover.</p>
+            <p>Found this on <a href="https://www.sitepoint.com/css-image-reveal-animation/">sitepoint article</a>
         </article>
     </section>
     <h2>Navigation</h2>
